70046174baf8d3f83fbd1d3ac7f6a35bc48d089e
Raymond Hammarling <raykay.rh@gmail.com>
2020-10-15 04:51:49 +0200

    Add maze generator command

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from maze_generator import make_maze
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/maze', methods=['GET', 'POST'])
+def generate_maze():
+    return {
+        'message': f'```\n{make_maze()}\n```'
+    }
